4a95d4c8cc3bc556e17df33dd8957538d43df1bf
Andrea Calderon <andrea.calderon.zuluaga@gmail.com>
2021-04-28 19:35:40 -0500

    Practica completa

---
+++ src/codigo/Tokens.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+/**
+ *
+ * @author Andrea Calderon & Karen Grisales
+ */
+public enum Tokens {
+    Reservadas,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Identificador,
+    Numero,
+    Modulo,
+    Equivalencia,
+    Diferente,
+    MenorIgual,
+    MayorIgual,
+    Menor, 
+    Mayor,
+    Caracter,
+    Separador,
+    ERROR
+}
